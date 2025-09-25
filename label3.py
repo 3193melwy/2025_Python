e26762de8d225659a5c174c963b8d07731cee68e
3193melwy <333melwy@gmail.com>
2025-09-25 11:06:28 +0900

    두번쨰

--- label3.py
+++ label3.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-photo = PhotoImage(file = "dog2.gif")
+photo = PhotoImage(file = "c:\2025pythonprogram2\Cinamonroll icon dark.jpeg")
 label = Label(root, image=photo)
 label.pack()
 root.mainloop()
