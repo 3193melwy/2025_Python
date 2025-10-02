687a71cc63ca43ad7ca60e1e6bd144898f660797
3193melwy <333melwy@gmail.com>
2025-10-02 11:14:45 +0900

    키보드 이벤트 처리까지

---
+++ 2025-10-02/grid.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("300x200")
+
+button1 = Button(root, text="버튼 1", bg = "red", fg="white")
+button1.grid(row=0, column=0)
+
+button2 = Button(root, text="버튼 2", bg = "green", fg="black")
+button2.grid(row=0, column=1)
+button3 = Button(root, text="버튼 3", bg = "blue", fg="white")
+button3.grid(row=1, column=0)
+button1 = Button(root, text="버튼 4", bg = "yellow", fg="red")
+button1.grid(row=1, column=1)
+
+root.mainloop()
